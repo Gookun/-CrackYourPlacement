1e728ec7dae5c8412268e21b8acc127bd4b2324b
Gookun <kunalgrg986@gmail.com>
2023-07-04 13:04:11 +0530

    bruteforce

--- 1. Set Matrix Zeroes.py
+++ 1. Set Matrix Zeroes.py
@@ -1,2 +1,9 @@
+"""
+Bruteforce approach
+step 1:  traverse the matrix till youn encounter 0
+step 2: turn all the 1 of same row and col into -1 and not the 0
+step 3: there whereever there is -1 you fill that up with zeroes.
+T(O): (N*M)(N+M)
+"""
 def SetmatrixZeroes():
     pass
