5563eb62ddfba55aa2b59538a6dfc255b177a5e0
Gookun <kunalgrg986@gmail.com>
2023-07-10 14:51:29 +0530

    q 15 done

---
+++ Arrays/15. Maximum Points You Can Obtain from Cards.py
@@ -0,0 +1,17 @@
+from typing import List
+def maxScore(cardPoints: List[int], k: int) -> int:
+    l, r = 0, len(cardPoints) - k
+    curSumWindow = sum(cardPoints[r:])
+    res = curSumWindow
+    while r < len(cardPoints):
+        curSumWindow += cardPoints[l] - cardPoints[r]
+        res = max(curSumWindow,res)
+        l += 1
+        r += 1
+    return res
+
+if __name__ == 'main':
+    cardPoints = [1,2,3,4,5,6,1]
+    k = 3
+    ans = maxScore(cardPoints,k)
+    print(ans)
