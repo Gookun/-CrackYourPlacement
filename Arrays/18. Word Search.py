3ccee92aa48293e1904186972c9567f64f7cacd3
Gookun <kunalgrg986@gmail.com>
2023-07-11 15:05:27 +0530

    Q 18 done

--- Arrays/18. Word Search.py
+++ Arrays/18. Word Search.py
@@ -1,5 +1,25 @@
 from typing import List
-
+# TC: O( n * m * 4^n)
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        pass
+        Rows, Cols = len(board), len(board[0])
+        path = set()
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or
+                r >= Rows or c >= Cols or
+                word[i] != board[r][c] or
+                (r, c) in path):
+                return False
+            path.add((r,c))
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r , c + 1, i + 1) or
+                   dfs(r , c - 1, i + 1))
+            path.remove((r,c))
+            return res
+        for r in range(Rows):
+            for c in range(Cols):
+                if dfs(r, c, 0): return True
+        return False
